9dfd3ea83f363be72244b2ba6d47d20450124129
Felipe Valencia <felipe@ocmiwc.com>
2021-08-02 12:58:44 -0500

    tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mode: process.env.NODE_ENV ? 'jit' : undefined,
-    purge: ["./src/**/*.tsx", "./public/index.html"],
+    purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
